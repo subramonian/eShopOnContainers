10fc88c8a6551e8cc4f18c86dc097857df7d344a
subramonian <subbusemail@gmail.com>
2019-08-15 01:08:08 -0400

    Update deploy-all.ps1

--- k8s/helm/deploy-all.ps1
+++ k8s/helm/deploy-all.ps1
@@ -4,12 +4,12 @@ Param(
     [parameter(Mandatory=$false)][string]$dockerPassword,
     [parameter(Mandatory=$false)][string]$externalDns,
     [parameter(Mandatory=$false)][string]$appName="eshop",
-    [parameter(Mandatory=$false)][bool]$deployInfrastructure=$true,
+    [parameter(Mandatory=$false)][bool]$deployInfrastructure=$false,
     [parameter(Mandatory=$false)][bool]$deployCharts=$true,
     [parameter(Mandatory=$false)][bool]$clean=$true,
-    [parameter(Mandatory=$false)][string]$aksName="",
-    [parameter(Mandatory=$false)][string]$aksRg="",
-    [parameter(Mandatory=$false)][string]$imageTag="latest",
+    [parameter(Mandatory=$false)][string]$aksName,
+    [parameter(Mandatory=$false)][string]$aksRg,
+    [parameter(Mandatory=$false)][string]$imageTag="dev",
     [parameter(Mandatory=$false)][bool]$useLocalk8s=$false
     )
 
